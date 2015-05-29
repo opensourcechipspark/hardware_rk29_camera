a1328b9b0ec6ac6782dd5a444e0401e22a33c45c
opensourcechipspark <opensource@chipspark.com>
2015-05-29 16:19:57 +0800

    popmetal init commit

---
+++ Config/Android.mk
@@ -0,0 +1,29 @@
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(strip $(TARGET_BOARD_PLATFORM)),rk3288)
+include $(CLEAR_VARS)
+LOCAL_PREBUILT_LIBS := libisp_silicomimageisp_api.so
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_MULTI_PREBUILT)
+
+
+endif
+
+ifeq ($(strip $(TARGET_BOARD_PLATFORM)),rk312x)
+
+   include $(CLEAR_VARS)
+    LOCAL_PREBUILT_LIBS := libisp_silicomimageisp_api.so
+    LOCAL_MODULE_TAGS := optional
+    include $(BUILD_MULTI_PREBUILT)
+
+endif
+
+ifeq ($(strip $(TARGET_BOARD_PLATFORM)),rk3036)
+
+   include $(CLEAR_VARS)
+    LOCAL_PREBUILT_LIBS := libisp_silicomimageisp_api.so
+    LOCAL_MODULE_TAGS := optional
+    include $(BUILD_MULTI_PREBUILT)
+
+endif
+
